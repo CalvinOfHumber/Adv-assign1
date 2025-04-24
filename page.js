b47e53df5b5310d58fad6b797e5900a0532a3f76
CalvinOfHumber <123662241+CalvinOfHumber@users.noreply.github.com>
2025-04-24 17:59:46 -0400

    Add files via upload

---
+++ page.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Lab4button from '@/components/Lab4button';
+import Lab4form from '@/components/Lab4form';
+import Lab4hover from '@/components/Lab4hover';
+import Lab4items from '@/components/Lab4items';
+
+
+export default function Home() {
+  return (
+    
+        <div> <h1>Calvin's Lab 4</h1>
+         <Lab4button />  
+         <Lab4form/>  
+         <Lab4hover/> 
+         <Lab4items/>
+         </div>
+  );
+}
